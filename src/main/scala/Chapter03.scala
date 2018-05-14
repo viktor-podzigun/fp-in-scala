be4b479a080b0613620c53c72b90872a62b50c3b
Viktor Podzigun <viktor.podzigun@gmail.com>
2018-05-14 09:10:51 +0200

    Added exercise 3.2

--- src/main/scala/Chapter03.scala
+++ src/main/scala/Chapter03.scala
@@ -18,4 +18,18 @@ object Chapter03 {
       case _ => 101
     }
   }
+
+  /**
+    * Exercise 3.2
+    *
+    * Implement the function tail for removing the first element of a List.
+    * Note that the function takes constant time.
+    *
+    * What are different choices you could make in your implementation if the List is Nil?
+    * We'll return to this question in the next chapter.
+    */
+  def tail[A](xs: List[A]): List[A] = xs match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
 }
--- src/test/scala/Chapter03Spec.scala
+++ src/test/scala/Chapter03Spec.scala
@@ -1,10 +1,19 @@
 import Chapter03._
 import org.scalatest._
+import fpinscala.datastructures._
 
 class Chapter03Spec extends FlatSpec with Matchers {
 
-  "matchResult" should "" in {
+  "matchResult" should "equal 3" in {
     //when & then
     matchResult shouldBe 3
+  }
+
+  "tail" should "remove first element of a list" in {
+    //when & then
+    tail(List()) shouldBe Nil
+    tail(List(1)) shouldBe Nil
+    tail(List(1, 2)) shouldBe List(2)
+    tail(List(1, 2, 3)) shouldBe List(2, 3)
   }
 }
